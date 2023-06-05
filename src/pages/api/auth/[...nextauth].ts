7c72c544b69a9b96a8919b97fc8a240234d2c298
Asachuna <84565188+yugoka@users.noreply.github.com>
2023-06-05 17:46:33 +0900

    Revert "Update [...nextauth].ts"
    This reverts commit 2a813c60a02209a497fb7d5bfcc23ee756af610f.

--- src/pages/api/auth/[...nextauth].ts
+++
@@ -1,8 +0,0 @@
-import NextAuth from "next-auth";
-import { FirestoreAdapter } from "@next-auth/firebase-adapter";
-import { firestore } from "lib/firestore";
-
-export default NextAuth({
-  adapter: FirestoreAdapter(firestore),
-  // ...
-});
